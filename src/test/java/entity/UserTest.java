33d3b5b58b6884ed7fb809f2fc37007e6405c63c
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-21 11:12:15 -0300

    getAll

--- src/test/java/entity/UserTest.java
+++ src/test/java/entity/UserTest.java
@@ -1,5 +1,6 @@
 package entity;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,9 +8,9 @@ import org.junit.jupiter.api.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.Objects;
+import java.util.List;
 
-public class UserTest {
+class UserTest {
 
     EntityManagerFactory emf;
     EntityManager em;
@@ -20,26 +21,41 @@ public class UserTest {
         em = emf.createEntityManager();
     }
 
+    @AfterEach
+    void close() {
+        em.close();
+        emf.close();
+    }
+
     @Test
     void createUserTest() {
-        User user = new User("Paulo", "user@mail.com");
+        User user = new User("Jorge", "user@mail.com");
 
         em.getTransaction().begin();
         em.persist(user);
         em.getTransaction().commit();
 
-        em.close();
-        emf.close();
-
         System.out.println(user.getId());
-        Assertions.assertTrue(user.getId() != null);
+        Assertions.assertNotNull(user);
     }
 
     @Test
     void findUserTest() {
         User user = em.find(User.class, 1L);
         System.out.println(user);
-        Assertions.assertTrue(Objects.nonNull(user));
+        Assertions.assertNotNull(user);
+    }
+
+    @Test
+    void getAllUsersTest() {
+        String sql = "select u from User u";
+
+        List<User> users = em.createQuery(sql, User.class)
+                .setMaxResults(5)
+                .getResultList();
+
+        users.forEach(System.out::println);
+        Assertions.assertNotNull(users);
     }
 
 }
