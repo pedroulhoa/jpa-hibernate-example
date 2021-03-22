7e739e83d48b76d5843f7581819334b730947ae5
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-22 15:11:33 -0300

    class DAO

---
+++ src/main/java/persistence/DAO.java
@@ -0,0 +1,72 @@
+package persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class DAO<E> {
+
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Class<E> sentClass;
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory("exampledb");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public DAO() {
+        this(null);
+    }
+
+    public DAO(Class<E> sentClass) {
+        this.sentClass = sentClass;
+        em = emf.createEntityManager();
+    }
+
+    public DAO<E> openTransaction() {
+        em.getTransaction().begin();
+        return this;
+    }
+
+    public DAO<E> closeTransaction() {
+        em.getTransaction().commit();
+        return this;
+    }
+
+    public DAO<E> insert(E entity) {
+        em.persist(entity);
+        return this;
+    }
+
+    public void closeEntityManager() {
+        em.close();
+    }
+
+    public DAO<E> insertAtomic(E entity) {
+        return this.openTransaction().insert(entity).closeTransaction();
+    }
+
+    public List<E> getAll() {
+        if (sentClass == null) throw new UnsupportedOperationException("class null");
+        String sql = "select e from " + sentClass.getName() + " e";
+        TypedQuery<E> query = em.createQuery(sql, sentClass);
+        return query.getResultList();
+    }
+
+    public List<E> getAll(int quantity, int offset) {
+        if (sentClass == null) throw new UnsupportedOperationException("class null");
+
+        String sql = "select e from " + sentClass.getName() + " e";
+        TypedQuery<E> query = em.createQuery(sql, sentClass);
+        query.setMaxResults(quantity);
+        query.setFirstResult(offset);
+        return query.getResultList();
+    }
+
+}
