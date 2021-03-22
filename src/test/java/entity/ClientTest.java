cf423f224cc5e0c947a9d3b321ddd4316e8030de
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-22 17:02:54 -0300

    client test

---
+++ src/test/java/entity/ClientTest.java
@@ -0,0 +1,33 @@
+package entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import persistence.DAO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    DAO<Object> dao;
+
+    @BeforeEach
+    void dao() {
+        dao = new DAO<>(Object.class);
+    }
+
+    @Test
+    void insertClientAndSeatTest() {
+        Seat seat = new Seat("5B");
+        Client client = new Client("Lucas", seat);
+
+        dao.openTransaction()
+                .insert(seat)
+                .insert(client)
+                .closeTransaction()
+                .closeEntityManager();
+
+        assertNotNull(seat.getId());
+        assertNotNull(client.getId());
+    }
+
+}
