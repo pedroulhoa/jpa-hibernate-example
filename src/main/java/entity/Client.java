ad3639e318b073fea034a4588a5f4e76d0879628
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-22 17:02:45 -0300

    new entities

---
+++ src/main/java/entity/Client.java
@@ -0,0 +1,59 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "seat_id", unique = true)
+    private Seat seat;
+
+    public Client() {
+    }
+
+    public Client(String name, Seat seat) {
+        this.name = name;
+        this.seat = seat;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", seat=" + seat +
+                '}';
+    }
+}
---
+++ src/main/java/entity/Seat.java
@@ -0,0 +1,45 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_seat")
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Seat() {
+    }
+
+    public Seat(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
